ad8ecb70b96d59b1b7876c389b738fb2aad174c4
Alessandro Morrone <alemorro00@gmail.com>
2024-03-26 17:54:40 +0100

    Added routes
    Added routes and sidebar to render various element

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,69 @@
+/**
 import TestingUser from './components/TestingUser';
 import TestRBoot from './components/TestRBoot'
 import Orb from './components/Orb';
+import Sidebar from './components/Sidebar';
 import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 
 function App() {
 
    return(
-      <div className='app'>
+      <>
          <Orb/>
-      </div>
+         
+      </>
       
    )
    
 }
+*/
 
-export default App;
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+   createBrowserRouter,
+   RouterProvider,
+   Router,
+   Link,
+   Outlet,
+} from 'react-router-dom';
+import Products from "./routes/Products"
+import Home from "./routes/Home"
+import Reports from "./routes/Reports"
+import Sidebar from "./components/Sidebar";
+
+const AppLayout = () => (
+   <>
+   <Sidebar/>
+   <Outlet />
+   </>
+)
+
+const router = createBrowserRouter([
+   {
+      element: <AppLayout />,
+      children: [
+         {
+            path: "/",
+            element: <Home />
+         },
+         {
+            path: "products",
+            element: <Products />
+         },
+         {
+            path: "reports",
+            element: <Reports />
+         },
+      ],
+   },
+]);
+
+export default function App() {
+   return(
+      <RouterProvider router={router} fallbackElement={<p>Initial load...</p>}/>
+   )
+}
