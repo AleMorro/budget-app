b964b38e0b3cf245d192d68b457008eaada4504c
Alessandro Morrone <alemorro00@gmail.com>
2024-05-12 19:48:54 +0200

    Implement correct get API
    Corrected the way to fetch object from the frontend request

--- frontend/src/context/globalContext.jsx
+++ frontend/src/context/globalContext.jsx
@@ -1,4 +1,7 @@
-// globalContext.js
+/**
+ * Class to save and contain the various method that make
+ * request to the server
+ */
 
 import React, { createContext, useContext, useState } from "react";
 import axios from 'axios';
@@ -9,9 +12,74 @@ const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
    
-   const [users, setUsers] = useState([]);
-   const [error, setError] = useState(null);
+   const[incomes, setIncomes] = useState([])
+   const[expenses, setExpenses] = useState([])
+   const[error, setError] = useState(null)
 
+   // calculate expenses
+   const addExpense = async(expense) => {
+      const res = await axios.post(`${BASE_URL}addExpense`, expense)
+         .catch((err) => {
+            setError(err.message)
+         })
+      getExpenses(expense.user_id);
+   }
+
+   const getExpenses = async (user_id) => {
+      const res = await axios.get(`${BASE_URL}expenses/?id=${user_id}`)
+      setExpenses(res.data)
+      console.log(res.data)
+   }
+
+   // to implement: deleteExpense
+
+   const totalExpenses = () => {
+      let total = 0;
+      expenses.forEach((expense) => {
+         total += expense.amount
+      })
+
+      return total;
+   }
+
+   // calculate incomes
+   const addIncome = async(income) => {
+      const res = await axios.post(`${BASE_URL}addExpense`, income)
+         .catch((err) => {
+            setError(err.message)
+         })
+      getIncomes(income.user_id);
+   }
+
+   const getIncomes = async(user_id) => {
+      try {
+         const res = await axios.get(`${BASE_URL}incomes/${user_id}`)
+         console.log(res.data)
+         setIncomes(res.data)
+      
+      } catch(err) {
+         console.error('Error fetching incomes in context:', err);
+         setError(err);
+      }
+   }
+
+   // to implement: deleteIncome
+
+   const totalIncomes = () => {
+      let total = 0;
+      incomes.forEach((income) => {
+         total += income.amount
+      })
+      console.log("Total incomes: " + total)
+      return total;
+   }
+
+   // calculate the total balance
+   const totalBalance = () => {
+      return totalIncomes() - totalExpenses()
+   }
+   
+   /*
    const getUsers = async () => {
       try {
          const res = await axios.get(`${BASE_URL}users`);
@@ -22,9 +90,20 @@ export const GlobalProvider = ({ children }) => {
          setError(error);
       }
    };
+   */
 
    return (
-      <GlobalContext.Provider value={{ users, getUsers, error }}>
+      <GlobalContext.Provider value={{ 
+         addExpense,
+         getExpenses,
+         totalExpenses,
+         addIncome,
+         getIncomes,
+         totalIncomes,
+         totalBalance,
+         error,
+         setError
+      }}>
          {children}
       </GlobalContext.Provider>
    );
