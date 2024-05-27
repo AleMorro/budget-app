0c69ca67e309139f7888bd8e03ad568a31ca744d
Alessandro Morrone <alemorro00@gmail.com>
2024-05-27 11:08:07 +0200

    Fixed fetch Data
    Now it fetch data from backend only when render the '/app? path

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { GlobalProvider } from './context/globalContext.jsx'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
-      <GlobalProvider>
          <App />
-      </GlobalProvider>
    </React.StrictMode>
 );
